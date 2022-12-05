506bb4a5867f80627e77731c87a5c173c945dfa4
James Baxter <james.baxter@ed.ac.uk>
2022-12-05 12:54:26 +0000

    day 3 problem 2 complete

--- scripts/day3.py
+++ scripts/day3.py
@@ -27,8 +27,8 @@ def main():
 
     # Identify common characters
     commoners = [splitandfind(rucksack) for rucksack in rucksacks]
-    commoners_priorities = []
 
+    commoners_priorities = []
     for commoner in commoners:
         if commoner in priorities:
             commoners_priorities.append(priorities[commoner])
@@ -37,7 +37,18 @@ def main():
     sum_commoners_priorities = sum(commoners_priorities)
     print('The sum of the priorities of these item types is ', sum_commoners_priorities)
 
+    # Form groups
+    groups = list(zip(*[iter(rucksacks)]*3))
+    badges = [''.join(set(group[0]) & set(group[1]) & set(group[2])) for group in groups]  # &=intersection
 
+    badge_priorities = []
+    for badge in badges:
+        if badge in priorities:
+            badge_priorities.append(priorities[badge])
+
+    # Solution Problem 2
+    sum_badge_priorities = sum(badge_priorities)
+    print('The sum of the priorities of these item types is ', sum_badge_priorities)
 
 
 if __name__ == "__main__":
