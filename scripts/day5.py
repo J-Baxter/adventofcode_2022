d99a9b92b366674cef5990395fcd1eb9a5909415
James Baxter <james.baxter@ed.ac.uk>
2022-12-06 14:53:02 +0000

    day 5 problems 1&2 complete

--- scripts/day5.py
+++ scripts/day5.py
@@ -2,53 +2,79 @@
 # Day 1
 
 import pandas as pd
-import numpy as np
+from collections import deque
+from itertools import zip_longest
 import os
 import re
 
 os.chdir('/Users/s1506888/Documents/PhD4/adventofcode_2022')
 
 
-def main():
+def rearrange(grid, qty, origin, dest, type):
+    move = []
 
+    if type == 'singular':
+        for i in range(1, qty + 1):
+            move = grid[origin - 1].pop()
+            grid[dest - 1].append(move)
+
+    elif type == 'grouped':
+        for i in range(1, qty + 1):
+            move.append(grid[origin - 1].pop())
+
+        move_reversed = list(reversed(move))
+        grid[dest - 1].extend(move_reversed)
+
+    return grid
+
+
+def main():
     # Import data
     inputfile = './data/day5'
-    initial_layout = []
+    stacks = []
     procedure = []
 
     with open(inputfile, 'r') as filedata:
         lineslist = filedata.readlines()
 
-        filedata.close()
-
-
         for textline in (lineslist[:9]):
-            lst = [i.stip() for j in re.findall(regex, textline) for i in j if j]
+            stacks.insert(0, [textline[i] for i in range(1, len(textline), 4)])
 
-            initial_layout.append(lst)
+        stacks = list(zip_longest(*stacks, fillvalue=' '))
+        stacks_p1 = [deque([crate for crate in stack if crate != ' ']) for stack in stacks]
+        stacks_p2 = [deque([crate for crate in stack if crate != ' ']) for stack in stacks]
 
         for textline in (lineslist[10:]):
             procedure.append(textline)
 
     filedata.close()
 
-    # Format initial layout
-    initial_layout_prep = [re.split(r'\s{1,}') for x in initial_layout]
-
-
     # Format rearrangement procedures
     procedure_clean = pd.DataFrame()
-    for i in list(range(1, 501)):
-        cleaned = [float(s) for s in re.findall(r'-?\d+\.?\d*', procedure[i-1])]
-        cleaned_df = pd.DataFrame(data=cleaned).T
+    for i in range(0, len(procedure)):
+        cleaned = [float(s) for s in re.findall(r'-?\d+\.?\d*', procedure[i])]
+        cleaned_df = pd.DataFrame(data=cleaned, ).T.astype(int)
         procedure_clean = procedure_clean.append(cleaned_df)
 
     procedure_clean.columns = ['qty', 'origin', 'destination']
 
+    # Problem 1
+    for i in range(0, len(procedure_clean)):
+        moves = procedure_clean.iloc[i]
+        rearrange(stacks_p1, moves['qty'], moves['origin'], moves['destination'], type='singular')
 
+    # Top element in each stack (Solution 1)
+    top_element_p1 = [x.pop() for x in stacks_p1]
+    print('When moved individually, the top crates are: ', (''.join(top_element_p1)))
 
+    # Problem 2
+    for i in range(0, len(procedure_clean)):
+        moves = procedure_clean.iloc[i]
+        rearrange(stacks_p2, moves['qty'], moves['origin'], moves['destination'], type='grouped')
 
-
+    # Top element in each stack (Solution 1)
+    top_element_p2 = [x.pop() for x in stacks_p2]
+    print('When moved as groups, the top crates are: ', (''.join(top_element_p2)))
 
 
 if __name__ == "__main__":
